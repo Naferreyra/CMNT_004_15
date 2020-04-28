d483cb1cac2ff21d83fc16ea5324141461f4842e
Albertocanal <acanal@visiotechsecurity.com>
2020-04-28 14:43:29 +0200

    [FIX] custom_account: nuevo campo y traducciones

--- project-addons/custom_account/models/account_analytic.py
+++ project-addons/custom_account/models/account_analytic.py
@@ -5,4 +5,4 @@ from odoo import models, fields, api, _, exceptions
 class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
-    ref = fields.Many2one('res.users')
+    reference = fields.Many2one('res.users','Reference')
